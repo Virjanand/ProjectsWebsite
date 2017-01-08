7b00c35840fe14fb42cc064af4f72de87dd10839
Virjanand <Virjanand@virjanand.com>
2017-01-08 06:24:10 +0100

    Put the head part, that was the same in each page, into an included snippet called header.html and included it into all the php pages. Put the header with the logo and header line and the menu toggle, into an included snippet called content_header and included it into all the php pages.

--- Projects/LearningPython/LearningPython.php
+++ Projects/LearningPython/LearningPython.php
@@ -1,32 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Vir's Projects">
-  <meta name="author" content="Virjanand Panday">
 
-  <title>Projects</title>
-  <link rel="shortcut icon" href="../../images/favicon.ico">
+<!--Header -->
+<?php include("../../includes/header.html");?>
 
-  <!-- Bootstrap Core CSS -->
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom CSS -->
-  <link href="../../assets/stylesheets/simple-sidebar.css" rel="stylesheet">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Own CSS -->
-
-  <link rel="stylesheet" href="../../assets/stylesheets/main.css">
-</head>
 <body>
   <div id="wrapper">
     <header class="container group">
@@ -43,7 +20,7 @@
 
     <!--Navigation -->
     <?php include("../../includes/navigation.html");?>
-    
+
 <div class="container">
   <!--Learning Python -->
   <h2>Learning Python</h2>
