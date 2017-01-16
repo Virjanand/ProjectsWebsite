c2bdc84df91ae35a4197286ca884cf23ed625290
Virjanand <Virjanand@virjanand.com>
2017-01-16 05:47:29 +0100

    Changed index page and navigation to have only categories webdevelopment, data science, game development, programming languages and software development

---
+++ Projects/Main/programming_languages.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+
+<!--Header -->
+<?php include("../../includes/header.html");?>
+
+  <body>
+
+  <div id="wrapper">
+
+    <!--Content Header -->
+    <?php include("../../includes/content_header.html");?>
+
+<!--Navigation -->
+<?php include("../../includes/navigation.html");?>
+
+<div class="container">
+    <section class="grid">
+
+  <!--Projects Website -->
+      <div class="col-1-3">
+        <h2><a href="Projects/ProjectsWebsite/ProjectsWebsite.html">
+            Projects Website
+            </a>
+        </h2>
+        <p>Started: January 2016 <br />
+          <ul>
+            <li><a href="http://virjanand.com/ProjectsWebsite/Projects/Main/index.php">
+              Website with projects overview</a><br />
+              Started: 3 January 2016 <br />
+              Technologies: HTML &amp; CSS
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Android -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningAndroid/LearningAndroid.html">
+            Learning Android</a></h2>
+        <p>Started: Feb 2016 <br />
+          <ul>
+            <li><a href="https://www.udemy.com/the-complete-android-developer-course/learn/#/">
+              Udemy course by Rob Percival</a> <br />
+              Started: 22 February 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Java -->
+    <div class="col-1-3">
+      <h2><a href="Projects/LearningJava/LearningJava.html">Learning Java</a></h2>
+      <p>Started: Apr 2016 <br />
+        <ul>
+          <li><a href="https://www.udemy.com/learn-java-by-building-projects/learn/v4/overview">
+            Udemy course by Eduonix Learning Solutions</a> <br />
+            Started: 13 Apr 2016 <br />
+
+          </li>
+        </ul>
+      </p>
+    </div>
+
+  <!--Unity3D -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningUnity3D/LearningUnity3D.html">
+            Learning Unity 3D</a>
+        </h2>
+        <p>Started: December 2015 <br />
+          <ul>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/procedural-cave-generation-tutorial">
+              Cave generator</a> <br />
+              Started: 28 April 2016 <br />
+              Finished: 6 July 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial">
+              Scavanger</a> <br />
+              Started: 26 March 2016 <br />
+              Finished: 27 April 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/tanks-tutorial">
+              Tanks</a> <br />
+              Started: 3 March 2016 <br />
+              Finished: 25 March 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/mini-projects">
+              Mini-projects</a> <br />
+              Started: 27 January 2016 <br />
+              Finished: 3 March 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/survival-shooter-project">
+              Survival shooter</a> <br />
+              Started: 12 January 2016 <br />
+              Finished: 27 January 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/space-shooter-tutorial">
+              Space shooter</a> <br />
+              Started: 10 December 2015 <br />
+              Finished: 12 January 2016
+            </li>
+            <li><a href="http://unity3d.com/learn/tutorials/projects/roll-ball-tutorial">
+              Roll a ball</a> <br />
+              Started: 7 December 2015 <br />
+              Finished: 10 December 2015
+            </li>
+          </ul>
+        </p>
+      </div>
+
+  <!--Unreal -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningUnreal/LearningUnreal.html">
+            Learning Unreal</a></h2>
+        <p>Started: May 2016 <br />
+          <ul>
+            <li><a href="https://www.udemy.com/unrealcourse/learn/v4/overview">
+              Udemy course by Ben Tristem</a> <br />
+              Started: 2 May 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+
+  <!--Javascript -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningJavascript/LearningJavascript.html">
+            Learning Javascript</a></h2>
+        <p>Started: November 2015 <br />
+          Following these two roadmaps: <br />
+          <a href="http://code.tutsplus.com/tutorials/the-best-way-to-learn-javascript--net-21954">
+            "The best way to learn Javascript"</a><br />
+          <a href="http://sixrevisions.com/javascript/learning-javascript-for-free/">
+            "A Roadmap to Learning JavaScript for Free"</a><br />
+          <ul>
+            <li><a href="http://eloquentjavascript.net/">
+              Eloquent Javascript</a> <br />
+              Started: 4 January 2016 <br />
+
+            </li>
+            <li><a href="https://www.codeschool.com/courses/javascript-road-trip-part-1">
+              Codeschool</a> <br />
+              Started: 27 December 2015 <br />
+              Finished: 30 December 2015
+            </li>
+            <li><a href="https://www.codecademy.com/learn/javascript">
+              Codecademy</a> <br />
+              Started: 29 November 2015 <br />
+              Finished: 27 December 2015
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Python -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningPython/LearningPython.html">
+            Learning Python</a></h2>
+        <p>Started: Apr 2016 <br />
+          <ul>
+            <li><a href="https://www.codecademy.com/learn/python">
+              Codecademy course on Python</a> <br />
+              Started: 10 Apr 2016 <br />
+              Finished: 10 Jun 2016
+            </li>
+            <li><a href="http://learnpythonthehardway.org/book/">
+              'Learn Python the Hard Way' by Zed Shaw</a> <br />
+              Started: 11 Jun 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Ruby on Rails -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningRubyOnRails/LearningRubyOnRails.html">
+            Learning Ruby on Rails</a></h2>
+        <p>Started: Apr 2016 <br />
+          <ul>
+            <li><a href="https://www.udemy.com/the-complete-ruby-on-rails-developer-course/learn/v4/overview">
+              Udemy course on Ruby on Rails by Mashrur Hossain</a> <br />
+              Started: 30 Apr 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Webdevelopment -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningWebdevelopment/LearningWebdevelopment.html">
+            Learning Webdevelopment</a></h2>
+        <p>Started: December 2015 <br />
+          <ul>
+            <li><a href="https://www.udemy.com/complete-web-developer-course/learn/#/">
+              Udemy course by Rob Percival</a> <br />
+              Started: 1 January 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--General Programming -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningGeneralProgramming/LearningGeneralProgramming.html">
+            Learning General Programming</a></h2>
+        <p>Started: April 2016 <br />
+          <ul>
+            <li><a href="http://www.ccs.neu.edu/home/matthias/HtDP2e/">
+              How to design Programs by Matthias Felleisen et al.</a> <br />
+              Started: 23 Apr 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--HTML & CSS -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningHTMLAndCSS/LearningHTMLAndCSS.html">
+            Learning HTML and CSS</a></h2>
+        <p>Started: December 2015 <br />
+          Following this roadmap: <br />
+          <a href="http://www.webdesigndegreecenter.org/learn-to-code/">
+            "Learn to code from scratch - A web development roadmap for beginners"</a><br />
+          <ul>
+            <li><a href="http://learn.shayhowe.com/html-css/">
+              Learn to code HTML &amp; CSS by Shay Howe</a> <br />
+              Started: 27 December 2015 <br />
+
+            </li>
+            <li><a href="https://www.codecademy.com/learn/web">
+              Codecademy</a> <br />
+              Started: 16 August 2015 <br />
+              Finished: 31 August 2015
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--iOS -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningiOSAppsSwift/LearningiOSAppsSwift.html">
+            Learning iOS apps/Swift</a></h2>
+        <p>Started: December 2015 <br />
+           Paused: February 2016
+          <ul>
+            <li><a href="https://www.udemy.com/the-complete-ios-9-developer-course/learn/#/">
+              Udemy course by Rob Percival</a> <br />
+              Started: 7 December 2015 <br />
+              Paused: 13 February 2016
+            </li>
+          </ul>
+        </p>
+      </div>
+  <!--Mendix -->
+      <div class="col-1-3">
+        <h2><a href="Projects/RelearningMendix/RelearningMendix.html">
+            (Re)Learning Mendix
+            </a>
+        </h2>
+        <p>Started: January 2016 <br />
+           Paused: April 2016
+          <ul>
+            <li><a href="https://www.mendix.com/blog/creating-custom-widgets-mendix/">
+              Mendix Widgets</a> <br />
+              Started: 5 March 2016 <br />
+              Paused: 15 April 2016
+            </li>
+            <li><a href="https://gettingstarted.mendixcloud.com/index.html">
+              Mendix Introduction Course</a> <br />
+              Started: 14 January 2016 <br />
+              Finished: 4 March 2016
+            </li>
+            <li><a href="https://gettingstarted.mendixcloud.com/index.html">
+              Mendix Getting Started</a> <br />
+              Started: 11 January 2016 <br />
+              Finished: 14 January 2016
+            </li>
+          </ul>
+        </p>
+      </div>
+    </section>
+
+  </div>
+</div>
+
+  <!--Javascript -->
+  <?php include("../../includes/javascript_at_bottom.html");?>
+
+  </body>
+</html>
---
+++ Projects/Main/software_development.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<!--Header -->
+<?php include("../../includes/header.html");?>
+
+  <body>
+
+  <div id="wrapper">
+
+    <!--Content Header -->
+    <?php include("../../includes/content_header.html");?>
+
+<!--Navigation -->
+<?php include("../../includes/navigation.html");?>
+
+<div class="container">
+    <section class="grid">
+
+  <!--General Programming -->
+      <div class="col-1-3">
+        <h2><a href="Projects/LearningGeneralProgramming/LearningGeneralProgramming.html">
+            Learning General Programming</a></h2>
+        <p>Started: April 2016 <br />
+          <ul>
+            <li><a href="http://www.ccs.neu.edu/home/matthias/HtDP2e/">
+              How to design Programs by Matthias Felleisen et al.</a> <br />
+              Started: 23 Apr 2016 <br />
+
+            </li>
+          </ul>
+        </p>
+      </div>
+  
+    </section>
+
+  </div>
+</div>
+
+  <!--Javascript -->
+  <?php include("../../includes/javascript_at_bottom.html");?>
+
+  </body>
+</html>
